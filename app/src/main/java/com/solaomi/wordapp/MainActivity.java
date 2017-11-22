560028ab458dce6104589a4ab94f0a073b3b4c94
sola <solaocodes@gmail.com>
2017-11-21 19:34:11 -0500

    Add an AsyncTask to lookup word of the day

--- app/src/main/java/com/solaomi/wordapp/MainActivity.java
+++ app/src/main/java/com/solaomi/wordapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.solaomi.wordapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import net.jeremybrooks.knicker.KnickerException;
+import net.jeremybrooks.knicker.WordsApi;
 import net.jeremybrooks.knicker.dto.WordOfTheDay;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +16,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WordOfTheDayAsyncTask task = new WordOfTheDayAsyncTask();
+        task.execute();
+    }
+
+    private void updateUI(WordOfTheDay wordObj) {
+
         String word = "example";
+
+        if ( wordObj != null ) { word = wordObj.getWord(); }
 
         TextView exampleTextView = (TextView) findViewById(R.id.example);
         exampleTextView.setText(word);
+    }
 
-        WordOfTheDay wordObj;
+    private class WordOfTheDayAsyncTask extends AsyncTask<Void, Void, WordOfTheDay> {
+
+        @Override
+        protected WordOfTheDay doInBackground(Void... voids) {
+
+            System.setProperty("WORDNIK_API_KEY", getString(R.string.wordnik_api_key));
+
+            try {
+                return WordsApi.wordOfTheDay();
+            } catch (KnickerException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(WordOfTheDay wordObj) {
+            updateUI(wordObj);
+        }
+
     }
 }
