5f6ea03e2a042d75fd13d48a79487728e34bdda1
sola <solaocodes@gmail.com>
2017-12-04 22:18:21 -0500

    Switch from Word of the Day AsyncTask to a Loader

--- app/src/main/java/com/solaomi/wordapp/SearchActivity.java
+++ app/src/main/java/com/solaomi/wordapp/SearchActivity.java
@@ -1,29 +1,31 @@
 package com.solaomi.wordapp;
 
+import android.app.LoaderManager;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.SearchView;
 import android.widget.TextView;
 
-// Library for Wordnik API (http://developer.wordnik.com/).
-import net.jeremybrooks.knicker.KnickerException;
-import net.jeremybrooks.knicker.WordsApi;
 import net.jeremybrooks.knicker.dto.WordOfTheDay;
 
+public class SearchActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<WordOfTheDay> {
 
-public class SearchActivity extends AppCompatActivity {
+     /** Tag for log messages */
+    private static final String LOG_TAG = SearchActivity.class.getName();
+
+    /** Constant value for the Word of the Day loader ID. */
+    private static final int WORD_OF_THE_DAY_LOADER_ID = 1;
+
+    private String mWordOfTheDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-        // Kick off an {@link AsyncTask} to perform network request.
-        WordOfTheDayAsyncTask task = new WordOfTheDayAsyncTask();
-        task.execute();
 
         // Find View that shows Word-of-the-Day.
         final TextView wordOfTheDayTextView = findViewById(R.id.word_of_the_day);
@@ -54,6 +56,13 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        // A reference to the LoaderManager, in order to interact with loaders.
+        LoaderManager loaderManager = getLoaderManager();
+
+        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+        // because this activity implements the LoaderCallbacks interface).
+        loaderManager.initLoader(WORD_OF_THE_DAY_LOADER_ID, null, this);
     }
 
     /**
@@ -64,11 +73,11 @@ public class SearchActivity extends AppCompatActivity {
     private void updateUI(WordOfTheDay wordOfTheDay) {
 
         // Word from wordOfTheDay object.
-        String word = wordOfTheDay.getWord();
+        mWordOfTheDay = wordOfTheDay.getWord();
 
         // Find reference to the {@link TextView} in the layout and set text to Word-of-the-Day.
         TextView exampleTextView = findViewById(R.id.word_of_the_day);
-        exampleTextView.setText(word);
+        exampleTextView.setText(mWordOfTheDay);
     }
 
     /**
@@ -86,36 +95,19 @@ public class SearchActivity extends AppCompatActivity {
         startActivity(wordIntent);
     }
 
-    /**
-     * {@link AsyncTask} to perform the network request on a background thread, and then
-     * update the UI with WordOfTheDay Object.
-     */
-    private class WordOfTheDayAsyncTask extends AsyncTask<Void, Void, WordOfTheDay> {
+    @Override
+    public Loader<WordOfTheDay> onCreateLoader(int i, Bundle bundle) {
+        return new WordOfTheDayLoader(this);
+    }
 
-        @Override
-        protected WordOfTheDay doInBackground(Void... voids) {
+    @Override
+    public void onLoadFinished(Loader<WordOfTheDay> loader, WordOfTheDay wordOfTheDay) {
+        updateUI(wordOfTheDay);
+    }
 
-            // Set API Key as a system property.
-            System.setProperty("WORDNIK_API_KEY", getString(R.string.wordnik_api_key));
-
-            // Perform HTTP request and recieve JSON response back (converted to WordOfTheDay object
-            // by Knicker Library)
-            try {
-                return WordsApi.wordOfTheDay();
-            } catch (KnickerException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        /**
-         *  Update the screen with information from the given given WordOfTheDay object (which is
-         *  the result of {@link WordOfTheDayAsyncTask}).
-         */
-        @Override
-        protected void onPostExecute(WordOfTheDay wordOfTheDay) {
-            updateUI(wordOfTheDay);
-        }
-
+    @Override
+    public void onLoaderReset(Loader<WordOfTheDay> loader) {
+        // Loader reset, so we can clear out existing data.
+        mWordOfTheDay = "";
     }
 }
---
+++ app/src/main/java/com/solaomi/wordapp/WordOfTheDayLoader.java
@@ -0,0 +1,45 @@
+package com.solaomi.wordapp;
+
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import net.jeremybrooks.knicker.KnickerException;
+import net.jeremybrooks.knicker.WordsApi;
+import net.jeremybrooks.knicker.dto.WordOfTheDay;
+
+/** Loads Word-of-the-Day by using an AsyncTask to perform the request */
+
+public class WordOfTheDayLoader extends AsyncTaskLoader<WordOfTheDay> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = WordOfTheDayLoader.class.getName();
+
+    public WordOfTheDayLoader(Context context) {
+        super(context);
+
+        // Set API Key as a system property.
+        System.setProperty("WORDNIK_API_KEY", context.getString(R.string.wordnik_api_key));
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * Fetches WordOfTheDay object on a background thread.
+     */
+    @Override
+    public WordOfTheDay loadInBackground() {
+        // Perform HTTP request and recieve JSON response back (converted to WordOfTheDay object
+        // by Knicker Library)
+        try {
+            return WordsApi.wordOfTheDay();
+        } catch (KnickerException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
