ca8ad6faaf60b7830cd4f72e9f7e5c41300e28e8
sola <solaocodes@gmail.com>
2017-12-15 18:55:15 -0500

    Create DefinitionsAdapter to display a list of Definition objects

--- app/src/main/java/com/solaomi/wordapp/DefinitionsFragment.java
+++ app/src/main/java/com/solaomi/wordapp/DefinitionsFragment.java
@@ -25,7 +25,7 @@ public class DefinitionsFragment extends Fragment {
     //    private static final String LOG_TAG = DefinitionsFragment.class.getName();
     private static final int DEFINITIONS_LOADER_ID = 1;
     private String mWord;
-    private ArrayAdapter<String> mAdapter;
+    private DefinitionsAdapter mAdapter;
 
     public DefinitionsFragment() {
         // Required empty public constructor
@@ -42,12 +42,13 @@ public class DefinitionsFragment extends Fragment {
             mWord = bundle.getString("word");
         }
 
-        // Create a new adapter that takes an empty list of Strings as input.
-        mAdapter = new ArrayAdapter<>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                new ArrayList<String>()
-        );
+//        // Create a new adapter that takes an empty list of Strings as input.
+//        mAdapter = new DefinitionsAdapter(
+//                getActivity(),
+//                android.R.layout.simple_list_item_1,
+//                new ArrayList<String>()
+//        );
+//        mAdapter = new DefinitionsAdapter(getActivity(), null);
 
         // Implement a loadercallback for the Definitions loader.
         LoaderCallbacks<List<Definition>> definitionsLoaderListener =
@@ -59,26 +60,20 @@ public class DefinitionsFragment extends Fragment {
 
                     @Override
                     public void onLoadFinished(Loader<List<Definition>> loader, List<Definition> data) {
-                        // Create a list of definitions.
-                        ArrayList<String> definitions = new ArrayList<>();
-
-                        for (Definition d : data) {
-                            definitions.add(d.getPartOfSpeech() + " - " + d.getText());
-                        }
-
                         // Clear the adapter of previous definitions data
                         mAdapter.clear();
 
-                        mAdapter.addAll(definitions);
+                        // Instantiate a {@link DefinitonsAdapter}, whose data source is a list of
+                        // {@link Definition}s. The adapter knows how to create list items for each
+                        // item in the list.
+                        mAdapter = new DefinitionsAdapter(getActivity(), data);
 
                         ListView listView = rootView.findViewById(R.id.list);
-
                         listView.setAdapter(mAdapter);
                     }
 
                     @Override
                     public void onLoaderReset(Loader<List<Definition>> loader) {
-                        // Loader reset, so we can clear out our existing data.
                         mAdapter.clear();
                     }
                 };
